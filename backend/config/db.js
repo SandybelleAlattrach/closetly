e178457370177099a90953e48969ce2fbb5b8d88
SandybelleAlattrach <sandybellealattrache@gmail.com>
2026-01-06 17:25:18 +0200

    add mysql2 dependency

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,18 @@
 import mysql from "mysql2/promise";
 
-export const db = mysql.createPool({
+export const db = await mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
 });
-
-export const testDB = async () => {
-  try {
-    const connection = await db.getConnection();
-    console.log("✅ MySQL connected");
-    connection.release();
-  } catch (err) {
-    console.error("❌ MySQL connection error:", err);
-    process.exit(1);
-  }
-};
+try {
+  const conn = await db.getConnection();
+  console.log("✅ MySQL connected (PUBLIC)");
+  conn.release();
+} catch (err) {
+  console.error("❌ MySQL connection error:", err);
+}
